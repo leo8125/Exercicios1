4baa7d2b5b4bcb6bbf4fc04df09f62d5e6de5860
leo8125 <72520188+leo8125@users.noreply.github.com>
2020-10-07 19:41:13 +0100

    Week_01

---
+++ Week_01/Frasesdodia/app/src/main/java/com/example/frasesdodia/MainActivity3.kt
@@ -0,0 +1,11 @@
+package com.example.frasesdodia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear)
+    }
+}
